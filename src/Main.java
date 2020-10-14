f838394c639b0f2173d54114ccba27a1540c8961
Andrew Drebot <andriy12345678910@gmail.com>
2020-10-15 02:05:57 +0300

    add: void method()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    public static void method(){
+        System.out.println("LOL method");
+    }
+
     public static void main(String[] args) {
 	System.out.println("LOL!");
 	System.out.println("KEK!");
