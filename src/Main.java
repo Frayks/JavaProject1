3f228abb8018724f6076225bfedc6c37ac61b973
Andrew Drebot <andriy12345678910@gmail.com>
2020-10-15 02:06:58 +0300

    add: void m()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+    public static void m(){
+        System.out.println("GG");
+        int a = 0;
+        System.out.println(a);
+    }
+
     public static void main(String[] args) {
 	System.out.println("LOL!");
 	System.out.println("KEK!");
