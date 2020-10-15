48bf529748b91745cb75f65257c77078fe0de547
Andrew Drebot <andriy12345678910@gmail.com>
2020-10-15 11:34:33 +0300

    add: void new_m()

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void method(){
+    public static void method() {
         System.out.println("LOL method");
     }
 
-    public static void m(){
+    public static void m() {
         System.out.println("GG");
         int a = 0;
         System.out.println(a);
     }
 
+    public static void new_m() {
+        Scanner scanner = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
-	System.out.println("LOL!");
-	System.out.println("KEK!");
+        System.out.println("LOL!");
+        System.out.println("KEK!");
     }
 }
